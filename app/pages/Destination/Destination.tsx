0b3f5d10026aa013e2822eb5d306ab0964c0c702
Reward Edoyugbo <r.edoyugbo@robiki.ai>
2024-11-11 10:02:58 +0100

    feat: Add Destination page component to the application

---
+++ app/pages/Destination/Destination.tsx
@@ -0,0 +1,11 @@
+import Container from "@app/components/layouts/Container/Container";
+
+const Destination = () => {
+  return (
+    <Container>
+      <div>Destination page</div>
+    </Container>
+  );
+};
+
+export default Destination;
